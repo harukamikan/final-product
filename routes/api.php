8a96cb54c2ef4e73d47faf881215e2c88ee4186c
harukamikan <s24b2040@bene.fit.ac.jp>
2025-12-04 11:13:48 +0900

    slackbot

--- routes/api.php
+++ routes/api.php
@@ -1,3 +1,8 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
 Route::post('/slack/test', function (Request $request) {
     \Log::info('Slack command received:', $request->all());
     
