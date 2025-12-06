cf403f62d7201ee72b92afa556864f4ec046ccd2
muraryou <ryo1009ta61m@example.com>
2025-12-06 17:56:12 +0900

    活動記録ボタンをgoals/createにリンク

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -34,8 +34,8 @@
 
             <!-- 新しい活動を記録ボタン -->
             <div class="mb-6">
-                <a href="/activities/create" class="inline-flex items-center px-4 py-2 bg-indigo-600 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-indigo-700">
-                    ➕ 新しい活動を記録
+                <a href="/goals/create" class="inline-flex items-center px-4 py-2 bg-indigo-600 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-indigo-700">
+                  ➕ 新しい活動を記録
                 </a>
             </div>
 
