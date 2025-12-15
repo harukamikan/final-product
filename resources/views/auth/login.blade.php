d9ccc41b0be181feb2cdece0f27fe8ff31fc471c
sansan296 <sanshizu429@gmail.com>
2025-12-16 06:12:12 +0900

    アカウントの新規登録ボタンが表示されるように編集

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -78,11 +78,26 @@
               py-2 rounded-md hover:bg-[#3a0f3d] shadow">
                         Slackでログイン
                     </a>
+                </div>
+
+                {{-- 新規登録導線 --}}
+                <div class="mt-6 text-center">
+                    <p class="text-sm text-gray-600">
+                        まだアカウントをお持ちでない方は
+                    </p>
+
+                    <a
+                        href="{{ route('register') }}"
+                        class="inline-block mt-3 px-6 py-2 border border-indigo-600 text-indigo-600 rounded-md
+           hover:bg-indigo-50 transition">
+                        新規登録
+                    </a>
+                </div>
+
+
+
             </div>
-
-
         </div>
-    </div>
     </div>
 
 </x-guest-layout>
