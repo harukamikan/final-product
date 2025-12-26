aeb50c3be02879edfebf4af48383fdd05c198ac1
sansan296 <sanshizu429@gmail.com>
2025-12-26 21:23:23 +0900

    ガチャ画面作成

--- app/Http/Controllers/GachaController.php
+++ app/Http/Controllers/GachaController.php
@@ -7,6 +7,15 @@ use Illuminate\Support\Facades\Auth;
 
 class GachaController extends Controller
 {
+    public function index()
+    {
+        $user = Auth::user();
+
+        $canDrawGacha = $user->miles > 0; // 条件は自由に調整OK
+
+        return view('gacha.index', compact('canDrawGacha'));
+    }
+
     public function draw(GachaService $gacha)
     {
         $user = Auth::user();
