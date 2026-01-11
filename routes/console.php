e2b28a1f3f8f3b026276e7403ba950f7fc619ec0
muraryou <ryo1009ta61m@icloud.com>
2026-01-12 00:33:35 +0900

    エラー修正

--- routes/console.php
+++ routes/console.php
@@ -11,6 +11,6 @@ Artisan::command('inspire', function () {
 // リマインド機能のスケジュール（テスト用：毎分）
 Schedule::command('reminders:send')
     ->everyMinute()
-    ->withoutOverlapping();
+    ->withoutOverlapping()
     ->sendOutputTo(storage_path('logs/reminders.log'));
     
