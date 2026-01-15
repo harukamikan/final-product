696c565dcb449a030f10a7c0348238d13b73ec6a
sansan296 <sanshizu429@gmail.com>
2026-01-15 16:21:39 +0900

    タブをファビコンに適応

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,6 +5,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Halfway</title>
+    <link rel="icon" href="/favicon.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
 
     {{-- Vite（Tailwind + Alpine + JS） --}}
     @vite(['resources/css/app.css', 'resources/js/app.js'])
